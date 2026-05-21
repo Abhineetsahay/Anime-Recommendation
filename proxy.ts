dbb9054a6b445c405b3e822008a57b8058b29f2b
Abhineetsahay <abhineetsahay@gmail.com>
2026-05-22 01:23:10 +0530

    improves UX bug

--- proxy.ts
+++ proxy.ts
@@ -2,13 +2,20 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { verifyToken } from "@/lib/auth";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
 
   if (token) {
     const payload = verifyToken(token);
 
-    if (payload && request.nextUrl.pathname === "/") {
+    if (
+      payload &&
+      (
+        request.nextUrl.pathname === "/" ||
+        request.nextUrl.pathname === "/login"||
+        request.nextUrl.pathname === "/signup"
+      )
+    ) {
       return NextResponse.redirect(
         new URL("/discover", request.url)
       );
